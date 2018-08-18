e6d80b87a4c9cd22ba1a02423729b1d2e1532f8e
wcss <wcss2010@qq.com>
2018-08-18 12:01:32 +0800

    ...........

--- SerialPortLib/SerialPort.cs
+++ SerialPortLib/SerialPort.cs
@@ -177,14 +177,6 @@ namespace SerialPortLib
                 {
                     if (SerialPortObject.IsOpen)
                     {
-                        try
-                        {
-                            SerialPortObject.Write(new byte[] { }, 0, 0);
-                        }
-                        catch (Exception ex)
-                        {
-                            SerialPortObject.Close();
-                        }
                         continue;
                     }
                     else
